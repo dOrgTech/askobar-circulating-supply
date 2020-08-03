46870291d6fa68d8d32c478a3f81105384518839
Nestor Amesty <nestor09amesty@gmail.com>
2020-08-04 01:42:18 +0200

    Text plain format corrected

--- src/middlewares/format.ts
+++ src/middlewares/format.ts
@@ -3,6 +3,7 @@ import { Request, Response } from "express";
 export const formatResponse = (req: Request, res: Response) => {
   
   if(req.query.format && req.query.format === 'text') {
+    res.set('Content-Type', 'text/plain')
     res.send(req.value)
   }
 
